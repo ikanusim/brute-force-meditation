ae8dc82a0f42f5ada652a5e998d7d58d655ddcd0
Daniel Hüsch <daniel.huesch@gmail.com>
2010-01-09 10:47:36 +0100

    animate character map in pre-computed groups for performance

--- public/js/ifzenelse.js
+++ public/js/ifzenelse.js
@@ -2,12 +2,25 @@ function setText(target) {
   $(target).text(String.fromCharCode(Math.floor(Math.random() * 65536)));
 }
 
+var groups = [];
+function populate_groups(count) {
+  all = [];
+  for (var i = 0; i < $('.letter').size(); i++)
+    all.push(i);
+  all = all.sort(function() { return Math.random() > 0.5 });
+  for (var i = 0; i < count; i++)
+    groups[i] = all.splice(0, $('.letter').size() / count);
+}
+
 function animate() {
-  setText($('.letter:eq(' + Math.floor(Math.random() * $('.letter').size()) + ')'));
-  setTimeout('animate();', 10);
+  $(groups[Math.floor(Math.random() * groups.length)]).each(function() {
+    setText($('.letter:eq(' + this + ')'));
+  });
+  setTimeout('animate();', 100);
 }
 
 $(function() {
+  populate_groups(10);
   animate();
   $('.letter').each(function() {
     setText(this);
