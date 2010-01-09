ac9e1bace7add0f6fe0f401f667d4097425a21f8
Daniel Hüsch <daniel.huesch@gmail.com>
2010-01-09 10:00:33 +0100

    animated version of random character map

--- public/js/ifzenelse.js
+++ public/js/ifzenelse.js
@@ -2,7 +2,13 @@ function setText(target) {
   $(target).text(String.fromCharCode(Math.floor(Math.random() * 65536)));
 }
 
+function animate() {
+  setText($('.letter:eq(' + Math.floor(Math.random() * $('.letter').size()) + ')'));
+  setTimeout('animate();', 10);
+}
+
 $(function() {
+  animate();
   $('.letter').each(function() {
     setText(this);
     $(this).mouseover(function() { setText(this); });
